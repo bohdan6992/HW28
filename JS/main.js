95aca839fa86e31d4d2ea454fb5ac307b574778a
bohdan6992 <bohdanmaljar@gmail.com>
2021-09-11 12:46:57 +0300

    change 1

---
+++ JS/main.js
@@ -0,0 +1,15 @@
+let a=10
+let c=4
+let d=8
+// a+x*c*2-d=0
+let x=(-a+d)/(c*2)
+console.log('Value of X:', x)
+
+// ___________________________
+let plotArea=400;
+let plotSideLenght = Math.sqrt(plotArea);
+let sidesNumber =4;
+let plotsNumber=4
+let companibleFencesNumber=3
+let fenceLenght= (plotSideLenght*sidesNumber*plotsNumber)-(companibleFencesNumber*plotSideLenght);
+console.log('Fence lenght:', fenceLenght);
