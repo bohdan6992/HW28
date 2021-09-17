10a88adf97ba79627a805e480a5385e67f4dd444
bohdan6992 <bohdanmaljar@gmail.com>
2021-09-17 19:33:48 +0300

    change 1

--- JS/main.js
+++ JS/main.js
@@ -1,42 +1,17 @@
-// //_________#1________
-// let a = prompt ('Введите первое число', '');
-// let b = prompt ('Введите второе число', '');
-// let x = a-b;
-// if (x>0){
-//   alert(`Разница между числами составляет: ${x}. Первое число больше`);
-// }else if (x<0){
-//   alert(`Разница между числами составляет: ${x}. Второе число больше`);
-// }else{
-//   alert(`Разница между числами составляет: ${x}. Числа равны`);
-// }
+
+//______________#1_________________
+for(i=2; i<=14; i++){
+  let num = i%2;
+  if(num==0){
+    console.log(i);
+  }
+}
+for(i=33; i<=45; i++){
+  let num = i%2;
+  if(num!=0){
+    console.log(i);
+  }
+}
 
 
-// //_________#2________
-// let depositAmount = prompt ('Введите сумму вклада (грн)', '');
-// let annualRate = prompt ('Введите сумму годовой ставки (%)', '');
-// let depositMonthsNumber = 6;
-// let anualMonthsNumber = 12;
-// let annualIncome = (depositAmount*annualRate*(depositMonthsNumber/anualMonthsNumber))/100;
-// alert(`Доход по депозитному вкладу за 6 месяцев: ${annualIncome} гривен`);
 
-
-// //_________#3________
-// let a = prompt ('Введите первое число', '');
-// let b = prompt ('Введите второе число', '');
-// a>b?(
-//   alert('Первое число больше')
-// ):a<b?(
-//   alert('Второе число больше')
-// ):(
-//   alert('Числа равны')
-// )
-
-
-// //_________#4________
-// let plotArea = prompt ('Введите площадь участка', '');
-// let plotSideLenght = Math.sqrt(plotArea);
-// let plotsNumber = prompt ('Введите количество участков', '');
-// let companibleFencesNumber = plotsNumber -1;
-// let sidesNumber =4;
-// let fenceLenght= (plotSideLenght*sidesNumber*plotsNumber)-(companibleFencesNumber*plotSideLenght);
-// alert(`Длина забора равна - ${fenceLenght}`);
