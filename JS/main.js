e336a076c76a760b15bd0fa7db14f3f3856940b6
bohdan6992 <bohdanmaljar@gmail.com>
2021-09-21 18:44:18 +0300

    change 1

--- JS/main.js
+++ JS/main.js
@@ -1,11 +1,64 @@
-let simpleNumBool;
-for (let i = 1; i < 10000; i++) {
-  simpleNumBool = false;
+// //___________________#1_______________________+
+// const arrStrs1 =['a', 'b', 'c', 'd'];
+// let limit = arrStrs1.length;
+// console.log(arrStrs1);
 
-  for (let j = 2; j < i; j++) {
-    if (i % j != 0 && (i+1)%8 == 0 ){
-      simpleNumBool = true;
-    };
-  };
-  if (simpleNumBool) console.log( i+1 );
-}
+// const arrStrs2 =[];
+
+// for (let i=0; i<limit; i++){
+//   if (i % 2 ==0){
+//     arrStrs2[i]=arrStrs1[3]
+//   }else{
+//     arrStrs2[i]=arrStrs1[0]
+//   }
+// }
+// console.log(arrStrs2);
+
+// //___________________#2_______________________+
+// const arrStrs1 =['a', 'b', 'c', 'd'];
+// let limit = arrStrs1.length;
+// console.log(arrStrs1);
+
+// const arrStrs2 =[];
+
+// for (let i=0; i<limit; i++){
+//   arrStrs2[i]=arrStrs1[limit - (i+1)]
+// }
+// console.log(arrStrs2);
+
+// //___________________#3_______________________+
+// const arrStrs1 =['a', 'b', 'a', 'b'];
+// let limit = arrStrs1.length;
+// console.log(arrStrs1);
+
+// for (let i=0; i<limit; i++){
+//   if (i!=3){
+//      arrStrs1[i]=arrStrs1[(i+1)]
+//   }else{
+//      arrStrs1[i]=arrStrs1[1]
+//   }  
+// }
+// console.log(arrStrs1);
+
+// //___________________#4_______________________+
+// const arrStrs1 =['a', 'b', 'c', 'd'];
+// let limit = arrStrs1.length;
+// console.log(arrStrs1);
+// const arrStrs2 =[];
+
+// for (let i=0; i<limit; i++){
+//   arrStrs2[i]=arrStrs1[i]+(i+1)
+// }
+// console.log(arrStrs2);
+
+// //___________________#5_______________________+
+// const arrStrs1 =['a', 'b', 'c', 'd'];
+// let limit = arrStrs1.length;
+// console.log(arrStrs1);
+
+// const arrStrs2 =[];
+
+// for (let i=0; i<limit; i++){
+//   arrStrs2[i]=arrStrs1[i] + arrStrs1[limit - (i+1)]
+// }
+// console.log(arrStrs2);
