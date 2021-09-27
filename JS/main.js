ef60d93d4050aab47157b1c9037b87f32ad92df3
bohdan6992 <bohdanmaljar@gmail.com>
2021-09-27 09:52:31 +0300

    change 2

--- JS/main.js
+++ JS/main.js
@@ -1,93 +1,167 @@
 
 
 const arr = [1, 8, -3, 4];
-
 let limit = arr.length-1;
 
 // //______________#1______________________+
-// for (let i = 0; i < 99; i++){
-//   arr.push(0);
-// }
-
-// //______________#2______________________+
-// for (let i = 0; i < 99; i++){
-//   if(!(arr[i] > 0)){
+// const task1 =(x) =>{
+//   for (let i = 0; i <= 99; i++){
 //     arr.push(0);
 //   };
+//   console.log(arr);
 // }
+
+// task1(arr);
+
+// //______________#2______________________+
+// const task2 =(arr) =>{
+//   for (let i = 0; i < 99; i++){
+//     if(!(arr[i] > 0)){
+//       arr.push(0);
+//     };
+//   };
+//   console.log(arr);
+// };
+
+// task2(arr);
 
 // //______________#3______________________+
-// for (let i = 0; i < 99; i++){
-//   arr.unshift(0);
-// }
-
-// //______________#4______________________+
-// for (let i = 0; i <= 99; i++){
-//   if(!(arr[i] > 0)){
+// const task3 =(arr) =>{
+//   for (let i = 0; i <= 99; i++){
 //     arr.unshift(0);
 //   };
+//   console.log(arr);
 // }
+
+// task3(arr);
+
+
+// //______________#4______________________+
+// const task4 =(arr) =>{
+//   for (let i = 0; i < 99; i++){
+//     if(!(arr[i] > 0)){
+//       arr.unshift(0);
+//     };
+//   };
+
+//   console.log(arr);
+// };
+
+// task4(arr);
 
 // //______________#5______________________+
-// for (let i = 0; i <= limit; i++){
-//   for (i=0; i < limit * 20; i++){
-//     arr.push(arr[i]);
+// const task5 =(arr) =>{
+//   for (let i = 0; i <= limit; i++){
+//     for (i=0; i < limit * 20; i++){
+//       arr.push(arr[i]);
+//     };
 //   };
-// }
+//   for (let i = 0; i <=limit; i++){
+//     arr.shift();
+//   };
+//   console.log(arr);
+// };
+
+// task5(arr);
 
 // //______________#6______________________+
-// for (let i = 0; i < 25; i++){
-//   arr.splice(1, 0, 0);
-// }
+// const task6 =(arr) =>{
+//   for (let i = 0; i < 25; i++){
+//     arr.splice(1, 0, 0);
+//   };
+//   console.log(arr);
+// };
+
+// task6(arr);
 
 // //______________#7______________________+
-// var sum = 0;
+// const task7 =(arr) =>{
+//   var sum = 0;
 
-// for (var i = 0; i <=limit; i++){
-//   sum += arr[i];
-// }
+//   for (var i = 0; i <=limit; i++){
+//     sum += arr[i];
+//   };
 
-// arr.push(sum);
+//   arr.push(sum);
+//   console.log(arr);
+// };
+
+// task7(arr);
+
 
 // //______________#8______________________+
-// var sumPositve = 0;
-// var sumNegative = 0;
+// const task8 =(arr) =>{
+//   var sumPositve = 0;
+//   var sumNegative = 0;
 
-// for (var i = 0; i <=limit; i++){
-//   if(arr[i] > 0){
-//     sumPar ++;
-//   }else{
-//     sumOdd ++;
+//   for (var i = 0; i <=limit; i++){
+//     if(arr[i] > 0){
+//       sumPositve ++;
+//     }else{
+//     sumNegative ++;
+//     };
 //   };
-// }
 
-// arr.push(sumPar);
-// arr.push(sumOdd);
+//   arr.push(sumPositve);
+//   arr.push(sumNegative);
+  
+//   console.log(arr);
+// };
+
+// task8(arr);
+
+
 
 // //______________#9______________________+
-// for (let i = 0; i <=limit; i++){
-//   arr.push(arr[i]*2);
-// }
-// for (let i = 0; i <=limit; i++){
-//   arr.shift();
-// }
+// const task9 =(arr) =>{
+//   var sumPositve = 0;
+//   var sumNegative = 0;
+
+//   for (let i = 0; i <=limit; i++){
+//     arr.push(arr[i]*2);
+//   };
+//   for (let i = 0; i <=limit; i++){
+//     arr.shift();
+//   };
+  
+//   console.log(arr);
+// };
+
+// task9(arr);
+
+
 
 // //______________#10______________________+
-// for (let i = 0; i <=limit; i++){
-//   arr.push(arr[i]+3);
-// }
+// const task10 = (arr) =>{
+//   for (let i = 0; i <=limit; i++){
+//     arr.push(arr[i]+3);
+//   }
+  
+//   console.log(arr);
+// };
+
+// task10(arr);
 
 // //______________#11______________________+
-// arr.push(arr[0]);
+// const task11 = (arr) =>{
+//   arr.push(arr[0]);
+  
+//   console.log(arr);
+// };
+
+// task11(arr);
 
 // //______________#12______________________+
-// for (let i = 0; i < limit; i++){
-//   arr.push(arr[i]+arr[limit]);
-// }
-// for (let i = 0; i < limit+1; i++){
-//   arr.shift();
-// }
+// const task12 = (arr) =>{
+//   for (let i = 0; i < limit; i++){
+//     arr.push(arr[i]+arr[limit]);
+//   };
+//   for (let i = 0; i < limit+1; i++){
+//     arr.shift();
+//   };
+  
+//   console.log(arr);
+// };
 
-
-console.log(arr);
+// task12(arr);
 
