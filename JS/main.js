3d5634cf0bec029454490c6a5e45ee98fd604523
bohdan6992 <bohdanmaljar@gmail.com>
2021-09-27 09:05:33 +0300

    change 1

--- JS/main.js
+++ JS/main.js
@@ -1,60 +1,93 @@
 
-// //___________________1_____________________________
-// const arr1 = [56, 6, 3, 88, 23, -11];
+
+const arr = [1, 8, -3, 4];
+
+let limit = arr.length-1;
+
+// //______________#1______________________+
+// for (let i = 0; i < 99; i++){
+//   arr.push(0);
+// }
+
+// //______________#2______________________+
+// for (let i = 0; i < 99; i++){
+//   if(!(arr[i] > 0)){
+//     arr.push(0);
+//   };
+// }
+
+// //______________#3______________________+
+// for (let i = 0; i < 99; i++){
+//   arr.unshift(0);
+// }
+
+// //______________#4______________________+
+// for (let i = 0; i <= 99; i++){
+//   if(!(arr[i] > 0)){
+//     arr.unshift(0);
+//   };
+// }
+
+// //______________#5______________________+
+// for (let i = 0; i <= limit; i++){
+//   for (i=0; i < limit * 20; i++){
+//     arr.push(arr[i]);
+//   };
+// }
+
+// //______________#6______________________+
+// for (let i = 0; i < 25; i++){
+//   arr.splice(1, 0, 0);
+// }
+
+// //______________#7______________________+
+// var sum = 0;
+
+// for (var i = 0; i <=limit; i++){
+//   sum += arr[i];
+// }
+
+// arr.push(sum);
+
+// //______________#8______________________+
+// var sumPositve = 0;
+// var sumNegative = 0;
+
+// for (var i = 0; i <=limit; i++){
+//   if(arr[i] > 0){
+//     sumPar ++;
+//   }else{
+//     sumOdd ++;
+//   };
+// }
+
+// arr.push(sumPar);
+// arr.push(sumOdd);
+
+// //______________#9______________________+
+// for (let i = 0; i <=limit; i++){
+//   arr.push(arr[i]*2);
+// }
+// for (let i = 0; i <=limit; i++){
+//   arr.shift();
+// }
+
+// //______________#10______________________+
+// for (let i = 0; i <=limit; i++){
+//   arr.push(arr[i]+3);
+// }
+
+// //______________#11______________________+
+// arr.push(arr[0]);
+
+// //______________#12______________________+
+// for (let i = 0; i < limit; i++){
+//   arr.push(arr[i]+arr[limit]);
+// }
+// for (let i = 0; i < limit+1; i++){
+//   arr.shift();
+// }
 
 
-// const castlingItems = (arrIndex1, arrIndex2) => {
-//   let limit = arr1.length;
-//   const arr2 = [];
-//   console.log(`Inlet array: [${arr1}]`);
-
-//   for (let i=0; i<limit; i++){
-//     arr2[i] = arr1[i];
-//   }
-
-//   if (arrIndex1 < limit && arrIndex2 < limit){
-//     arr2[arrIndex1] = arr1[arrIndex2];
-//     arr2[arrIndex2] = arr1[arrIndex1];
-//     console.log(`Resultative array: [${arr2}]`);
-//   }
-//   else{
-//     console.log(`Sorry, but the indexes are outside the array`);
-//   }
-// };
-
-// castlingItems(4, 3);
-
-
-// //___________________2_____________________________
-// const arr1 = [56, 6, 3, 88, 23, -11];
-
-// const shiftItem = (arrIndex, direction) => {
-//   let limit = arr1.length;
-//   console.log(`Inlet array: [${arr1}]`);
-//   const arr2 = [];
-
-//   for (let i = 0; i < limit; i++){
-//     arr2[i] = arr1[i];
-    
-//   }
-
-//   if (direction == 'right' && arrIndex < limit - 1){
-//     arr2[arrIndex + 1] = arr1[arrIndex];
-//     arr2[arrIndex] = arr1[arrIndex + 1];
-//     console.log(`Resultative array: [${arr2}]`);
-//   }
-//   if (direction == 'left' && arrIndex < limit - 1 && arrIndex > 0){
-//     arr2[arrIndex] = arr1[arrIndex + 1];
-//     arr2[arrIndex + 1] = arr1[arrIndex];
-//     console.log(`Resultative array: [${arr2}]`);
-//   }
-//   if (arrIndex > limit){
-//     console.log(`Sorry, but the index ${arrIndex} is outside the array`);
-//   }
-//   if (direction != 'right' && direction != 'left'){
-//     console.log(`Sorry, but you can only select "right" or "left"`);
-//   }
-// };
-
-// shiftItem(2, 'left');
+console.log(arr);
 
