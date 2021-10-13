1fbb3d1f027999acf8f7f2b8376af291f3cd7b5d
bohdan6992 <bohdanmaljar@gmail.com>
2021-10-13 18:49:27 +0300

    change 1

--- JS/main.js
+++ JS/main.js
@@ -1,21 +1,68 @@
-const btn = document.querySelector('.search_btn');
-const main = document.querySelector('.main');
+const btnLeft = document.querySelector('.left')
+const btnRight = document.querySelector('.right')
+const btnBack = document.querySelector('.back')
+const btnForward = document.querySelector('.forward')
+const box = document.querySelector('.box')
 
-const generateBoxes = () => {
-  const text = document.querySelector('.input');
-  const tempArr = text.value.split(' ')
-  let tempStr = '';
-  let longest = '';
-  for(let i = 0; i < tempArr.length; i++){
-    tempStr = `
-    ${tempStr}
-    <input class="out" placeholder='${tempArr[i]}'>`
-    if (tempArr[i].length > longest.length){
-      longest = tempArr[i];
+
+btnLeft.addEventListener('mousedown', () => {
+  box.classList.toggle('moveLeft')
+});
+btnLeft.addEventListener('mouseup', () => {
+  box.classList.remove('moveLeft')
+});
+
+btnRight.addEventListener('mousedown', () => {
+  box.classList.toggle('moveRight')
+});
+btnRight.addEventListener('mouseup', () => {
+  box.classList.remove('moveRight')
+});
+
+btnBack.addEventListener('mousedown', () => {
+  box.classList.toggle('moveBack')
+});
+btnBack.addEventListener('mouseup', () => {
+  box.classList.remove('moveBack')
+});
+
+btnForward.addEventListener('mousedown', () => {
+  box.classList.toggle('moveForward')
+});
+btnForward.addEventListener('mouseup', () => {
+  box.classList.remove('moveForward')
+});
+
+
+document.addEventListener('keydown', (event) => {
+  if (event.key === ' '){
+    box.classList.add('magnif')
+  }
+});
+document.addEventListener('keyup', (event) => {
+  if (event.key === ' '){
+    box.classList.remove('magnif')
+  }
+});
+
+const keyClickFunc = (key1, key2, way) => {
+  document.addEventListener('keydown', (event) => {
+    if (event.key === `${key1}`|| event.code === `${key2}`){
+      box.classList.toggle(`${way}`)
+    };
+  });
+  document.addEventListener('keyup', (event) => {
+    if (event.key === `${key1}`|| event.code === `${key2}`){
+      box.classList.remove(`${way}`)
     }
-  };
-  main.innerHTML = tempStr
-  console.log(`The longest word: '${longest}'`);
+  });
 };
 
-btn.addEventListener('click', generateBoxes);
+keyClickFunc('w', 'ArrowUp', 'moveForward');
+keyClickFunc('s', 'ArrowDown', 'moveBack');
+keyClickFunc('a', 'ArrowLeft', 'moveLeft');
+keyClickFunc('d', 'ArrowRight', 'moveRight');
+
+
+
+
